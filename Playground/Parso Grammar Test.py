fe47b5cf6f5714b954bb5f06889a81dde766a74e
BrendanMesters <brendanmesters@gmail.com>
2020-05-28 18:27:10 +0200

    Added a `Yogurt.py` and a `YogurtGrammar.txt` file which contain some sample Yogurt code and a moddified version of the Python grammar respectively. I also played around a bit more with the `printParseTree` function in `Parso Grammar Test.py` as it seemed to withold some possibly important information in the way it was designed previously.

--- Playground/Parso Grammar Test.py
+++ Playground/Parso Grammar Test.py
@@ -3,18 +3,18 @@ import parso
 
 def printParseTree(tree, indent = 0):
     for node in tree.children:
-        if not str(node).__contains__("PythonNode"):
+        if True or not str(node).__contains__("PythonNode"):
             print(indent * "  " + str(node))
         if hasattr(node, "children"):
             printParseTree(node, indent + 1)
 
 
-with open("grammar38_alt.txt") as f:
+with open("grammar38.txt") as f:
     bnf_text = f.read()
 
 grammar = parso.grammar.PythonGrammar(parso.grammar.parse_version_string("3.8"), bnf_text)
 
-with open("Hello_world_alt.py") as f:
+with open("Hello_world.py") as f:
     program_text = f.read()
 
 parseTree = grammar.parse(program_text)
---
+++ Playground/Yogurt.py
@@ -0,0 +1,23 @@
+actor switch(dblink):
+    config:
+        #config
+    states:
+        state on: "/on"
+    actions:
+        self
+
+actor light(dblink, switch):
+    config:
+        #config
+    states:
+        illuminated: "/active"
+    actions:
+        trigger switch.on:
+            illuminated = switch.on
+
+
+kitchen_light = light("//data/devices/light[location='Kitchen']")
+kitchen_switch = switch("//data/sensor/switch[location='Kitchen']")
+
+
+kitchen_light.input(switch = kitchen_switch)
