f5e9746c9d2d38f318ecb71358f1839ca7b254bb
BrendanMesters <brendanmesters@gmail.com>
2020-06-04 22:01:55 +0200

    We (me and Jack) played around with different aspects of the language (mostly mentally). Most changes aren't to important here

--- Playground/Yogurt.py
+++ Playground/Yogurt.py
@@ -1,24 +1,48 @@
-actor switch(dblink):
+actor Switch():
     config:
         #config
     states:
         state on: "/on"
     actions:
-        trigger foobar:
 
 
-actor light(dblink, switch):
+actor Light(switch):
     config:
         #config
     states:
-        illuminated: "/active"
+        state illuminated: "/active"
     actions:
         trigger switch.on:
             illuminated = switch.on
 
-
-kitchen_light = light("//data/devices/light[location='Kitchen']")
-kitchen_switch = switch("//data/sensor/switch[location='Kitchen']")
+instantiate kitchen_switch: Switch "//data/sensor/switch[location='Kitchen']"
 
 
-kitchen_light.input(switch = kitchen_switch)
+bind kitchen_switch(params*)
+
+
+
+
+
+
+
+
+
+
+kitchen_switch = Switch: "//data/sensor/switch[location='Kitchen']"
+kitchen_light = Light: "//data/devices/light[location='Kitchen']"
+
+
+
+Light "//data/devices/light[location='Kitchen']"
+
+kitchen_light.bind(kitchen_switch)
+
+
+
+
+kitchen_switch = Switch()
+kitchen_light = Light(kitchen_switch)
+
+kitchen_switch("//data/sensor/switch[location='Kitchen']")
+kitchen_light.bind("//data/devices/light[location='Kitchen']")
